a0c1b4d692f39d8ffc9fc781645ad06002d70bc3
Kirby-PC\Kirby <travis.jordan031@gmail.com>
2017-10-22 23:45:02 -0600

    Gun not visible

--- Source/S05_TestingGrounds/Player/FirstPersonCharacter.h
+++ Source/S05_TestingGrounds/Player/FirstPersonCharacter.h
@@ -45,9 +45,16 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
 	FVector GunOffset;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<class AGun> GunBlueprint;
+
 	/** Whether to use motion controller location for aiming. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	uint32 bUsingMotionControllers : 1;
+
+private:
+
+	AGun* Gun;
 
 protected:
 	
